c9aae3d92edfe2bc3666a84ebc6ae391d4ddb725
chianw <chiancw@gmail.com>
2025-04-13 11:13:54 +0800

    fix syntax error

--- main.tf
+++ main.tf
@@ -3,10 +3,8 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_S3_bucket" "example" {
+resource "aws_s3_bucket" "example" {
   bucket = "tfcloud-awss31233"
-  acl    = "private"
-
   tags = {
     Name        = "MyExampleBucket"
     Environment = "Prod"
